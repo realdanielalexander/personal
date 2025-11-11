29bfd4950348ff74eb71a96269994bbb4ca32d03
CIS4190/5190 Staff <>
2025-11-11 00:21:21 -0500

    feat: changed copyright

--- components/Copyright.jsx
+++ components/Copyright.jsx
@@ -6,7 +6,12 @@ const Copyright = () => {
   return (
     <Section paddingY={8} display="flex" flexDirection="column" margin={0}>
       <Text textAlign={'center'} margin={0}>
-        &copy; 2024 Daniel Alexander | Last updated October 2024
+        &copy; 2025 Daniel Alexander | Last updated November 2025<br />
+        <Text fontSize={'sm'} color={'gray.500'}>
+          Built with Next.js, Chakra UI, and React. Hosted on Vercel.
+        </Text> 
+        <Text fontSize={'sm'} color={'gray.500'}>Color schemes inspired by monkeytype.com</Text>
+        
       </Text>
     </Section>
   )
